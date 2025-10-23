765b408b0ff698511a121dd224ec925eca7238bc
Efe Şener <efesener-milas@hotmail.com>
2025-10-24 00:36:12 +0300

    fix for null pointer exception on file reader

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,6 +5,6 @@ import service.FileManager;
 public class Main {
     static void main() {
         FileManager fm = new FileManager();
-        fm.readFile("/resource/data/for_devs");
+        fm.readFile("resource/data/for_devs");
     }
 }
--- src/service/FileManager.java
+++ src/service/FileManager.java
@@ -1,34 +1,25 @@
 package service;
 
-import java.io.FileReader;
-import java.io.InputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class FileManager {
-    public void readFile(String filePath){
-        String[] fileLines = {"init for test"};
-        try{
-                InputStream input = getClass().getResourceAsStream(filePath);
-                // i use them because if i package to .jar i can't read resources with classical method
-                BufferedReader br = new BufferedReader(new InputStreamReader(input));
-                fileLines = br.readAllAsString().split("\n");
-            if (fileLines == null){
-                FileReader fr = new FileReader(filePath);
-                String file = fr.readAllAsString();
-                fr.close();
-                fileLines = file.split("\n");
-                // i hope not expect read char to char and manually split and manually
+    public String[] readFile(String filePath){
+        String[] fileLines = {"File can't read"};
+        BufferedReader br;
+        File file = new File(filePath);
+        try {
+            if (file.exists()) {
+                br = new BufferedReader(new FileReader(filePath));
+            } else {
+                InputStream input = getClass().getResourceAsStream("/" + filePath);
+                br = new BufferedReader(new InputStreamReader(input));
             }
-        }catch (Exception e){
+            fileLines = br.readAllAsString().split("\n");
+        }
+        catch (Exception e){
             e.printStackTrace();
         }
-        finally{
-            for(String line : fileLines){
-            System.out.println(line);
-        }
-        }
-       
+        return fileLines;
     }
 }
 
